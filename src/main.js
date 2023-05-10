ea0e933def6b99e0a8c723f89f4dc4c8bb8c7288
diegogila <dgg26496@hotmail.com>
2023-05-09 21:23:22 -0600

    Se comienza con la logica del progr

--- src/main.js
+++ src/main.js
@@ -1,8 +1,45 @@
+const outputNode = document.querySelector(".calculator__output");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+let number;
+let operatorSelect = null;
 
 
-const select = (num) => {
-    const input = document.querySelector()
-};
+const selectNumber = (number) => {
+
+        outputNode.innerText += number;
+
+}
+
+const selectOperator = (operator) => {
+    if (operatorSelect == null) {
+        operatorSelect = operator;
+        outputNode.innerText = null;
+    } else{
+        const result = calc(parseInt(outputNode.innerText),5,operator);
+        console.log(result,operator);
+        outputNode.innerText = result;
+    }
+    
+}
+
+
+for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    element.addEventListener("click", () => {
+        selectNumber(element.innerText);
+    });
+    
+}
+
+for (let index = 0; index < operators.length; index++) {
+    const element = operators[index];
+    element.addEventListener("click", () => {
+        selectOperator(element.innerText);
+    });
+    
+}
+
 
 const calc = (num1,num2,arit) => {
     let result;
@@ -26,5 +63,3 @@ const calc = (num1,num2,arit) => {
     }
     return result;
 };
-
-console.log(calc(5,6,'-'));
