ef949f8cd570e41cfec7953761dc9980f17cf5b3
diegogila <dgg26496@hotmail.com>
2023-05-10 18:04:32 -0600

    Se termian con logica y se continua con estetica

--- src/main.js
+++ src/main.js
@@ -1,65 +1,90 @@
-const outputNode = document.querySelector(".calculator__output");
-const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operator");
-let number;
-let operatorSelect = null;
+//-----------Variables------------------------//
 
+let numberSaved = 0;
+let operatorSelect = null;
+let flag = 0;
+
+//-----------Functions-----------------------//
+
+const reset = () => {
+  numberSaved = 0;
+  operatorSelect = null;
+  flag = 0;
+  outputNode.innerText = null;
+};
 
 const selectNumber = (number) => {
-
-        outputNode.innerText += number;
-
-}
+  if (flag == 0) {
+    outputNode.innerText += number;
+  } else {
+    outputNode.innerText = number;
+    flag = 0;
+  }
+};
 
 const selectOperator = (operator) => {
-    if (operatorSelect == null) {
-        operatorSelect = operator;
-        outputNode.innerText = null;
-    } else{
-        const result = calc(parseInt(outputNode.innerText),5,operator);
-        console.log(result,operator);
-        outputNode.innerText = result;
+  console.log(operator, operatorSelect, numberSaved);
+  if (operatorSelect == null) {
+    operatorSelect = operator;
+    numberSaved = Number(outputNode.innerText);
+  } else {
+    const result = calc(
+      numberSaved,
+      Number(outputNode.innerText),
+      operatorSelect
+    );
+    outputNode.innerText = result;
+    if (operator != "=") {
+      operatorSelect = operator;
+    } else {
+      operatorSelect = null;
     }
-    
-}
+    numberSaved = result;
+  }
+  flag = 1;
+};
 
+const calc = (num1, num2, arit) => {
+  let result;
+  switch (arit) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "x":
+      result = num1 * num2;
+      break;
+    case "÷":
+      if ((num1 == 0) | (num2 == 0)) {
+        alert("No se puede dividir entre cero");
+      } else {
+        result = num1 / num2;
+      }
+      break;
+    default:
+      break;
+  }
+  return result;
+};
 
-for (let index = 0; index < numbers.length; index++) {
+//-----------------------Start Program-----------------------------------------//
+
+(() => {
+  restartButton.addEventListener("click", reset);
+
+  for (let index = 0; index < numbers.length; index++) {
     const element = numbers[index];
     element.addEventListener("click", () => {
-        selectNumber(element.innerText);
+      selectNumber(element.innerText);
     });
-    
-}
+  }
 
-for (let index = 0; index < operators.length; index++) {
+  for (let index = 0; index < operators.length; index++) {
     const element = operators[index];
     element.addEventListener("click", () => {
-        selectOperator(element.innerText);
+      selectOperator(element.innerText);
     });
-    
-}
-
-
-const calc = (num1,num2,arit) => {
-    let result;
-    switch (arit) {
-        case "+":
-            result = num1 + num2;
-            break;
-
-        case "-":
-            result = num1 - num2;
-            break;
-        case "*":
-            result = num1 * num2;
-            break;
-        case "/":
-            result = num1 / num2;
-            break;
-        
-        default:
-            break;
-    }
-    return result;
-};
+  }
+})();
---
+++ src/nodes.js
@@ -0,0 +1,4 @@
+const outputNode = document.querySelector(".calculator__output");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const restartButton = document.getElementById("Reset");
